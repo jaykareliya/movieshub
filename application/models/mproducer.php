d6bdede93c3d629a502444d9ca3fb9e7025e9ccd
jay <jay@pinatech.biz>
2012-11-09 14:37:38 +0530

    change in producer
    change in whole file.
    case changes

--- application/models/mproducer.php
+++ application/models/mproducer.php
@@ -5,7 +5,7 @@ class Mproducer extends CI_Model
 
 	public function get_producer($movie_id)
 	{
-		$query="SELECT p.Producer_Name,p.Producer_Id FROM `movie` AS m LEFT JOIN movie_Producer AS mp ON ( m.movie_id = mp.movie_id ) LEFT JOIN Producer p ON (p.Producer_id = mp.Producer_id )WHERE m.movie_id = ". $movie_id;
+		$query="SELECT p.Producer_Name,p.Producer_Id FROM `movie` AS m LEFT JOIN movie_producer AS mp ON ( m.Movie_Id = mp.Movie_Id ) LEFT JOIN producer p ON (p.Producer_Id = mp.Producer_Id )WHERE m.Movie_Id = ". $movie_id;
 		$q=$this->db->query($query);
 
 		return $q->result();
@@ -14,7 +14,7 @@ class Mproducer extends CI_Model
 
 	public function get_producer_detail($producer_id)
 	{
-		$query="SELECT * FROM Producer WHERE producer_id =".$producer_id;
+		$query="SELECT * FROM producer WHERE Producer_Id =".$producer_id;
 		$q =$this->db->query($query);
 		
 		return $q->result();
@@ -24,9 +24,9 @@ class Mproducer extends CI_Model
 	
 	public function producer_see_all($producer_id,$start_row,$limit)
 	{
-		$query="SELECT count( movie_comment.movie_comment_id ) AS cnt,m.Movie_Id,m.Movie_Name,m.Movie_Image,m.Movie_Rating FROM movie_Producer AS mp LEFT JOIN movie AS m ON(m.movie_id = mp.movie_id)
-				LEFT JOIN `movie_comment` ON ( m.movie_id = movie_comment.movie_id )
-				WHERE mp.Producer_id=".$producer_id."
+		$query="SELECT count( movie_comment.movie_comment_id ) AS cnt,m.Movie_Id,m.Movie_Name,m.Movie_Image,m.Movie_Rating FROM movie_Producer AS mp LEFT JOIN movie AS m ON(m.Movie_Id = mp.Movie_Id)
+				LEFT JOIN `movie_comment` ON ( m.Movie_Id = movie_comment.Movie_Id )
+				WHERE mp.Producer_Id=".$producer_id."
 				group by m.Movie_Id,m.Movie_Name,m.Movie_Image,m.Movie_Rating
 		   		limit ".$start_row." ,".$limit;
 		$q =$this->db->query($query);
@@ -38,7 +38,7 @@ class Mproducer extends CI_Model
 
 	public function producer_count($producer_id)
 	{
-		$Q = $this->db->query("SELECT count(*) as cnt FROM `movie` AS m LEFT JOIN movie_Producer AS mp ON ( m.movie_id = mp.movie_id ) LEFT JOIN Producer p ON (p.Producer_id = mp.Producer_id )WHERE p.producer_id = ". $producer_id);
+		$Q = $this->db->query("SELECT count(*) as cnt FROM `movie` AS m LEFT JOIN movie_producer AS mp ON ( m.Movie_Id = mp.Movie_Id ) LEFT JOIN producer p ON (p.Producer_Id = mp.Producer_Id )WHERE p.Producer_Id = ". $producer_id);
 
 			
   		$cnt="";
