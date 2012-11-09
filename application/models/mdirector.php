910c17e9233be5b111c6d05362afac5c53a35228
jay <jay@pinatech.biz>
2012-11-09 14:30:12 +0530

    change in mdirector
    change in cases as database names

--- application/models/mdirector.php
+++ application/models/mdirector.php
@@ -4,7 +4,7 @@ class Mdirector extends CI_Model
 {
 	public function get_director($movie_id)
 	{
-		$query ="SELECT d.Director_Name,d.Director_Id FROM `movie` AS m LEFT JOIN movie_Director AS md ON ( m.movie_id = md.movie_id ) LEFT JOIN director d ON (d.director_id = md.director_id )WHERE m.movie_id = ". $movie_id;
+		$query ="SELECT d.Director_Name,d.Director_Id FROM `movie` AS m LEFT JOIN movie_director AS md ON ( m.Movie_Id = md.Movie_Id ) LEFT JOIN director d ON (d.Director_Id = md.Director_Id )WHERE m.Movie_Id = ". $movie_id;
 		$q = $this->db->query($query);
 
 		return $q->result();
@@ -14,7 +14,7 @@ class Mdirector extends CI_Model
 	
 	public function get_director_detail($director_id)
 	{
-		$query="SELECT * FROM director WHERE director_id =".$director_id;
+		$query="SELECT * FROM director WHERE Director_Id =".$director_id;
 		$q =$this->db->query($query);
 		
 		return $q->result();
@@ -23,9 +23,9 @@ class Mdirector extends CI_Model
 
 	public function director_see_all($director_id,$start_row,$limit)
 	{
-		$query="SELECT count( movie_comment.movie_comment_id ) AS cnt,m.Movie_Id,m.Movie_Name,m.Movie_Image,m.Movie_Rating FROM Movie_Director AS md LEFT JOIN movie AS m ON(md.movie_id = m.movie_id) 
-		 LEFT JOIN `movie_comment` ON ( m.movie_id = movie_comment.movie_id )
-		 WHERE md.director_id =".$director_id." group by m.Movie_Id,m.Movie_Name,m.Movie_Image,m.Movie_Rating Limit ".$start_row.",".$limit;
+		$query="SELECT count( movie_comment.Movie_Comment_Id ) AS cnt,m.Movie_Id,m.Movie_Name,m.Movie_Image,m.Movie_Rating FROM movie_director AS md LEFT JOIN movie AS m ON(md.Movie_Id = m.Movie_Id) 
+		 LEFT JOIN `movie_comment` ON ( m.Movie_Id = movie_comment.Movie_Id )
+		 WHERE md.Director_Id =".$director_id." group by m.Movie_Id,m.Movie_Name,m.Movie_Image,m.Movie_Rating Limit ".$start_row.",".$limit;
 		$q =$this->db->query($query);
 		
 		return $q->result();
@@ -34,7 +34,7 @@ class Mdirector extends CI_Model
 
 	public function director_see_all_($director_id)
 	{
-		$query="SELECT m.Movie_Id,m.Movie_Name,m.Movie_Image FROM Movie_Director AS md LEFT JOIN movie AS m ON(md.movie_id = m.movie_id) WHERE md.director_id =".$director_id;
+		$query="SELECT m.Movie_Id,m.Movie_Name,m.Movie_Image FROM movie_director AS md LEFT JOIN movie AS m ON(md.Movie_Id = m.Movie_Id) WHERE md.Director_Id =".$director_id;
 		$q =$this->db->query($query);
 		
 		return $q->result();
@@ -45,7 +45,7 @@ class Mdirector extends CI_Model
 	public function director_movie_count($director_id)
 	{
 
-		$Q = $this->db->query("SELECT count(*) as cnt FROM `movie` AS m LEFT JOIN movie_Director AS md ON ( m.movie_id = md.movie_id ) LEFT JOIN director d ON (d.director_id = md.director_id )WHERE d.director_id = ". $director_id);
+		$Q = $this->db->query("SELECT count(*) as cnt FROM `movie` AS m LEFT JOIN movie_director AS md ON ( m.Movie_Id = md.Movie_Id ) LEFT JOIN director d ON (d.Director_Id = md.Director_Id )WHERE d.Director_Id = ". $director_id);
 
 			
   		$cnt="";
